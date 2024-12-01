bab8ec45330d93ce2767883ff3def45ba75224ab
Jacopo Scazzosi <jacopo@scazzosi.com>
2024-12-01 21:17:57 +0100

    AbstractLevel@2.x: removes support for @nxtedition/rocks-level as it has yet to update to abstract-level@2.x

--- test/node.ts
+++ test/node.ts
@@ -1,11 +1,9 @@
 
 import { runMemoryLevelTests } from './backends/memorylevel.js';
 import { runClassicLevelTests } from './backends/classiclevel.js';
-import { runRocksLevelNXTEditionTests } from './backends/rockslevel-nxtedition.js';
 import { runOtherTests } from './others/others.js';
 
 runOtherTests();
 runMemoryLevelTests();
 runClassicLevelTests();
-runRocksLevelNXTEditionTests();
 
